37f27b129505f30a835af0cf8b01c3fa6eb80041
mhmmdq <mhmmdq@mhmmdq.ir>
2021-10-13 15:55:47 +0330

    Build Decoders

--- src/Decoder.php
+++ src/Decoder.php
@@ -2,6 +2,33 @@
 
 namespace Mhmmdq\Request;
 
-abstract class Decoder {
+class Decoder {
+    /**
+     * Decode JSON
+     *
+     * @return array
+     */
+    public static function decodeJson() {
+        $args = func_get_args();
+        $response = call_user_func_array('json_decode', $args);
+        if ($response === null) {
+            $response = $args['0'];
+        }
+        return $response;
+    }
+    /**
+     * Decode XML
+     *
+     * @return array
+     */
+    public static function decodeXml()
+    {
+        $args = func_get_args();
+        $response = @call_user_func_array('simplexml_load_string', $args);
+        if ($response === false) {
+            $response = $args['0'];
+        }
+        return $response;
+    }
     
 }
