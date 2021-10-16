934f486d77ae41475be5d64486a820625dcdbdc1
mhmmdq <mhmmdq@mhmmdq.ir>
2021-10-16 22:42:04 +0330

    Build the http_header_parse method

--- src/Decoder.php
+++ src/Decoder.php
@@ -30,5 +30,29 @@ class Decoder {
         }
         return $response;
     }
-    
+    /**
+     * Convert Http header string to array
+     *
+     * @param string $header
+     * @return array
+     */
+    public static function http_parse_headers(string $header){
+        
+        $headers = array();
+
+        $header_text = substr($header, 0, strpos($header, "\r\n\r\n"));
+
+        foreach (explode("\r\n", $header_text) as $i => $line)
+            if ($i === 0)
+                $headers['http_code'] = $line;
+            else
+            {
+                list ($key, $value) = explode(': ', $line);
+
+                $headers[$key] = $value;
+            }
+
+        return $headers;
+
+     }
 }
